62db2eb25ae82acaef8f15a40f9420d25adfc69a
onebig32 <335424774@qq.com>
2017-04-04 02:17:10 +0000

    add wikitten code

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// Example configuration file for Wikitten. To use it,
+// first rename it to `config.php`.
+
+// Custom name for your wiki:
+define('APP_NAME', '技术文档');
+
+// Set the filename of the automatic homepage here
+define('DEFAULT_FILE', 'index.md');
+
+// Custom path to your wiki's library:
+// define('LIBRARY', '/path/to/wiki/library');
+
+// Enable editing files through the interface?
+// NOTE: There's currently no authentication built into Wikitten, controlling
+// who does what is your responsibility.
+define('ENABLE_EDITING', true);
+
+// Enable JSON page data?
+define('USE_PAGE_METADATA', true);
+
+// Enable the dark theme here
+// define('USE_DARK_THEME', true);
+
+// Disable the Wikitten logo here
+define('USE_WIKITTEN_LOGO', false);
+
+// Enable PasteBin plugin ?
+// define('ENABLE_PASTEBIN', true);
+// define('PASTEBIN_API_KEY', '');
---
+++ library/项目/测试 AJ.md
@@ -0,0 +1,23 @@
+# My Wiki
+
+[Technology]()
+
+  * [Index...](Technology/index.md)
+  - - - -
+  * # [Linux]
+  * [Index...](Technology/Linux/index.md)
+  * [Debug](Technology/Linux/Debug/index.md)
+  - - - -
+  * # [Network]
+  * [Index...](Technology/Network/index.md)
+
+[Travel]()
+
+  * [Index...](Travel/index.md)
+
+[Blog](https://zohead.com/)
+
+[🔍]()
+
+  * # Search file name or content.
+  * [<input id="search_input" type="text"/>](#)
